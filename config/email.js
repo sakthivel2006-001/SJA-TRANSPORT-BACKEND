ddcc726d63164fb25a3a3dc1f2ef18673982ae38
Sakthivel <sakthivelsankar2006@gmail.com>
2026-07-21 23:56:03 +0530

    Add SMTP verification

--- config/email.js
+++ config/email.js
@@ -10,4 +10,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP Error:", error);
+  } else {
+    console.log("✅ SMTP Server is ready");
+  }
+});
+
 module.exports = transporter;
