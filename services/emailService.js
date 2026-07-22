27eed92e955c5169563adfb8b848ad6f7175aec8
Sakthivel <sakthivelsankar2006@gmail.com>
2026-07-22 11:33:46 +0530

    fix: use Gmail SMTP for owner booking notifications

--- services/emailService.js
+++ services/emailService.js
@@ -1,4 +1,5 @@
 const { sendEmail } = require('../utils/brevoEmail');
+const { sendOwnerNotification } = require('../utils/gmailNotify');
 
 /* Booking notification */
 const sendBookingNotification = async (booking) => {
@@ -36,9 +37,7 @@ const sendBookingNotification = async (booking) => {
       </div>
     `;
 
-  await sendEmail({
-    to: process.env.OWNER_EMAIL,
-    toName: 'Admin',
+  await sendOwnerNotification({
     subject,
     htmlContent,
   });
